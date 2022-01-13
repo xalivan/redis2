ac9b2dd55f239b7257090d251115d5beaabea2a0
xalivan <xalivan@ukr.net>
2022-01-13 14:24:59 +0200

    1.Commit

---
+++ src/main/java/com/example/redis3/Redis3Application.java
@@ -0,0 +1,13 @@
+package com.example.redis3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Redis3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Redis3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/redis3/Redis3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redis3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Redis3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
